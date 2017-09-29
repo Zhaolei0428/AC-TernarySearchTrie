97ed5981a9caebeb51bbcda9e3b5570ef63d8ae1
Zhaolei <1175108017@qq.com>
2017-09-29 21:45:57 +0800

    add ac_complement function to make faillink and outlink.

--- ac3.c
+++ ac3.c
@@ -2,19 +2,20 @@
 
 main()
 {
+//    Queue* q=que_init();
+//    printf("%d\n",q->head);
+	
 	AC_STRUCT *ACTree = ac_alloc();
 //	printf("%d",sizeof(*ACTree));
 	ACTree->psize = 0;
 	char* ch = "abcd";
 	char* ch1 = "aefg";
-	char* ch2 = "abche";
+	char* ch2 = "chae";
 	ac_add_string(ACTree, ch, 4, 1);
 	ac_add_string(ACTree, ch1, 4, 2);
-	if(ac_add_string(ACTree, ch2, 5, 3)==1)
-		preorder(ACTree->root->next);
-	else
-	    printf("error");
-	
+	ac_add_string(ACTree, ch2, 4, 3);
+	ac_implement(ACTree);
+	preorder(ACTree->root->next);
 } 
 
 
@@ -24,13 +25,60 @@ void preorder(TSTree node){
 	currentNode=node;
 	if(currentNode!=NULL)
 	{
-		printf("%c",currentNode->data);
+		printf("%c %d...",currentNode->data,currentNode->stateId);
+		if(currentNode->faillink!=NULL)
+			printf("%c %d\n",currentNode->faillink->data,currentNode->faillink->stateId);
+		else
+		    printf("\n");
 		preorder(currentNode->lchild);
 		preorder(currentNode->next);
 		preorder(currentNode->rchild);
 	}
 }
 
+//存储树节点的队列，辅助实现faillink
+Queue* que_init()
+{
+	Queue* q;
+	if((q=malloc(sizeof(Queue))) == NULL)
+		return NULL;
+	memset(q,0,sizeof(Queue));
+	q->head=0;
+	q->tail=0;
+	return q;
+}
+//队列判空 
+int empty(Queue* q)
+{
+	if(q->head==q->tail)
+		return 1;
+	return 0;
+}
+
+//入队，成功返回1，否则返回0 
+int enqueue(Queue* q, TSTree node)
+{
+	if((q->head-q->tail+maxn)%maxn==1)
+		return 0;
+	else
+	{
+		q->queue[q->tail]=node;
+		q->tail=(q->tail+1)%maxn;
+		return 1;
+    }
+} 
+//出队，返回出队节点或NULL（队空） 
+TSTree dequeue(Queue* q)
+{
+	if(q->head==q->tail)
+		return NULL;
+	else
+    {
+    	int head=q->head;
+    	q->head=(head+1)%maxn;
+    	return q->queue[head];
+	}
+}
 
 
 /*
@@ -184,9 +232,93 @@ int ac_add_string(AC_STRUCT *node, char *P, int M, int id)
     return 1;
 }
 
-
-
-
+//对trie树添加faillink，完成outputlink，实现自动机 
+int ac_implement(AC_STRUCT* node)
+{
+	TSTree root,currentNode,preNode,lchild,rchild,state,tempNode;
+	root=node->root;
+	int flag;
+	Queue* q1, *q2, *q3;
+	q1=que_init();      //主要队列 
+	q2=que_init();      //辅助队列 
+	q3=que_init();      //辅助队列 
+	if(root->next!=NULL)
+	{
+		currentNode=root->next;
+		if(!enqueue(q2,currentNode))
+			return 0;
+		while(!empty(q2))
+		{
+			currentNode=dequeue(q2);
+			currentNode->faillink=root;
+			enqueue(q1,currentNode);
+			lchild=currentNode->lchild;
+			rchild=currentNode->rchild;
+			if(lchild!=NULL)
+				enqueue(q2,lchild);
+			if(rchild!=NULL)
+				enqueue(q2,rchild);
+		}
+		
+		while(!empty(q1))
+		{
+			preNode=dequeue(q1);
+			if(preNode->next!=NULL)
+			{
+				enqueue(q2,preNode->next);
+				while(!empty(q2))
+				{
+					currentNode=dequeue(q2);
+					lchild=currentNode->lchild;
+			    	rchild=currentNode->rchild;
+			    	if(lchild!=NULL)
+				    	enqueue(q2,lchild);
+			    	if(rchild!=NULL)
+			     		enqueue(q2,rchild);
+			     	enqueue(q1,currentNode);
+			     	
+			     	//计算currentNode的faillink
+			     	flag=1;
+			     	state=preNode;
+					while(flag)
+					{
+						state = state->faillink;
+						if(state->next!=NULL)
+						{
+							enqueue(q3,state->next);
+							while(!empty(q3))
+							{
+								tempNode=dequeue(q3);
+								lchild=tempNode->lchild;
+								rchild=tempNode->rchild;
+								if(lchild!=NULL)
+									enqueue(q3,lchild);
+								if(rchild!=NULL)
+									enqueue(q3,rchild);
+								if(tempNode->data==currentNode->data)
+								{
+									currentNode->faillink=tempNode;
+									//计算outlink 
+									if(tempNode->stateId!=0)
+										currentNode->outlink=tempNode;
+									flag=0;
+								}
+							}
+						}
+						if(state==root&&flag==1)
+						{
+							currentNode->faillink=root;
+							break;
+						}
+					}//end while(flag) 
+				
+				}//end q2
+			}
+			
+		}//end q1
+	}
+	return 1;
+} 
 
 
 
--- ac3.h
+++ ac3.h
@@ -3,27 +3,44 @@
 #include<string.h>
 
 
+#define maxn 1000000
+
 //TernarySearchTrie三叉树的结构 
 typedef struct TSNode{
 	char data;  //节点存储的字节数据
 	struct TSNode *lchild, *rchild;     //同级字节TSNode 
 	struct TSNode* next;    //下一字节对应的TSNode 
-//	TSNode* failLink; 
+	struct TSNode* faillink;       //失效链接
+	struct TSNode* outlink;      //输出链接
+	 
 	int stateId;     //状态id
 	 
 } TSNode, *TSTree;
 
+typedef struct{
+	TSTree queue[maxn];
+	int head,tail;
+} Queue;
 
+Queue* que_init();
+//入队，成功返回1，否则返回0 
+int enqueue(Queue* q, TSTree node);
+TSTree dequeue(Queue* q);
+int empty(Queue* q);
 
 typedef struct {
   TSTree root;     //三叉树根节点 
-  int cNum;       //搜索串长度 
+  long startpoint, cNum;       //文件位置起始点，搜索串长度 
   int psize;      //模式串个数 
   char* T;        //搜索串序列 
-  TSTree currentNode;        //当前搜索到的节点 
+  TSTree currentNode, outlink;        //当前搜索到的节点,待输出节点 
   
 } AC_STRUCT;
 
 AC_STRUCT *ac_alloc(void);
 int ac_add_string(AC_STRUCT *node, char *P, int M, int id); 
 void preorder(TSTree node);
+
+//对trie树添加faillink，完成outputlink，实现自动机 
+//出现错误返回0，否则返回1 
+int ac_implement(AC_STRUCT* node); 
